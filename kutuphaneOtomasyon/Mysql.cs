2f3bc88734d125459b64737ae523b4833ac820a2
Furkan Ay <furkan-ay@outlook.com>
2015-11-27 00:51:42 +0200

    first commit

---
+++ kutuphaneOtomasyon/Mysql.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+/*
+    Author: Furkan Ay
+*/
+
+namespace kutuphaneTakip
+{
+    class Mysql
+    {
+        private string server = "localhost";
+        private string user = "root";
+        private string password = "1234";
+        private string database = "kutuphane";
+
+        private MySqlConnection conn;
+        private MySqlCommand cmd;
+        private MySqlDataReader reader;
+
+        public void Open()
+        {
+            conn = new MySqlConnection("server=" + this.server + ";uid=" + this.user
+                + ";pwd=" + this.password + ";database=" + this.database);
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException myEx)
+            {
+
+                throw myEx;
+            }
+
+        }
+        public void Close()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (MySqlException myEx)
+            {
+
+                throw myEx;
+            }
+        }
+
+        public MySqlDataReader Select(string query)
+        {
+            cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+            reader = cmd.ExecuteReader();
+            return this.reader;
+        }
+
+
+    }
+}
